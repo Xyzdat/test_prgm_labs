ce0c159d468703b5f59be1a72696695fa2c3227b
xyzdat <agolubev04@mail.ru>
2026-08-29 19:29:43 +0300

    add solution and describtions

--- test_progmatic_lab.py
+++ test_progmatic_lab.py
@@ -1,28 +1,25 @@
 digits = "9876543210"
 
-
+#создаём рекурсивную функцию для поиска выражения, которое равно 200
 def find_expression(index, expression):
-    # Если обработали все цифры
+    #проверяем если индекс будет равен длинне цифр, то проверяем выражение на равенство 200
     if index == len(digits):
         if eval(expression) == 200:
             print(expression + "=200")
             return True
         return False
 
-    # Пробуем три варианта:
-    # 1. ничего не ставить
+    #рекурсивнно проверяем и пытаемсядобавить текущую цифру к выражению без знака, с плюсом и с минусом
     if find_expression(index + 1, expression + digits[index]):
         return True
 
-    # 2. поставить '+'
     if find_expression(index + 1, expression + "+" + digits[index]):
         return True
 
-    # 3. поставить '-'
     if find_expression(index + 1, expression + "-" + digits[index]):
         return True
 
     return False
 
-
+#запускаем функцию с начальным индексом 1 и первым символом строки digits
 find_expression(1, digits[0])
